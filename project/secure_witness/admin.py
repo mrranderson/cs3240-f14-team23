1874f83e0bf57a8d86ad1cf45f779e112af29bde
NicholasLangemeier <ncl6cr@virginia.edu>
2014-12-03 01:24:59 -0500

    Admin set up.

--- project/secure_witness/admin.py
+++ project/secure_witness/admin.py
@@ -1,7 +1,32 @@
 from django.contrib import admin
+from django import forms
 from secure_witness.models import Folder, Bulletin
+from django.contrib.auth.models import User
 
-admin.site.register(Bulletin)
-admin.site.register(Folder)
+class BulForm(forms.ModelForm):
+    class Meta:
+        model=Bulletin
+        fields=()
+class BulletinAdmin(admin.ModelAdmin):
+    form=BulForm
+
+admin.site.register(Bulletin, BulletinAdmin)
+
+class FolForm(forms.ModelForm):
+    class Meta:
+        model=Folder
+        fields=()
+class FolderAdmin(admin.ModelAdmin):
+    form=FolForm
+
+class UForm(forms.ModelForm):
+    class Meta:
+        model=User
+        fields=()
+class UserAdmin(admin.ModelAdmin):
+    form=UForm
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
 
 # Register your models here.
